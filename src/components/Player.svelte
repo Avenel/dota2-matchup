8dbcb1af4530d420af2a65408c275e15416947c8
Avenel <martin.briewig@googlemail.com>
2024-09-07 19:54:30 +0200

    adds modal for clicking on hero

--- src/components/Player.svelte
+++ src/components/Player.svelte
@@ -9,11 +9,16 @@
 	let heroStats = HEROES_STATS.find((x) => x.id === player.hero_id);
 
 	let heroIcon = (heroStats: any) => 'https://cdn.cloudflare.steamstatic.com' + heroStats?.icon;
+	let heroImage = (heroStats: any) => 'https://cdn.cloudflare.steamstatic.com' + heroStats?.img;
+
+	let heroModal: HTMLDialogElement;
 </script>
 
 <tr>
 	<td>
-		<div class="flex flex-col">
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<!-- svelte-ignore a11y-no-static-element-interactions -->
+		<div class="flex flex-col" on:click={() => heroModal.showModal()}>
 			<div class="flex items-center {rtl ? 'flex-row-reverse' : 'flex-row'}">
 				<img
 					alt={hero?.name}
@@ -27,3 +32,33 @@
 		</div>
 	</td>
 </tr>
+
+<dialog bind:this={heroModal} class="modal modal-bottom">
+	<div class="modal-box">
+		<h3 class="text-lg font-bold flex flex-col items-center">
+			<div class="flex">
+				<img
+					alt={hero?.name}
+					src={heroIcon(heroStats)}
+					height="30px"
+					style="height:30px;"
+					class="my-0 mr-2"
+				/>
+				<div class="text-2xl font-bold mb-2">{hero?.localized_name}</div>
+			</div>
+			<img
+				alt={hero?.name}
+				src={heroImage(heroStats)}
+				height="100px"
+				style="height:100px; object-fit: contain;"
+				class="my-0 mr-2"
+			/>
+		</h3>
+
+		<div class="modal-action justify-center">
+			<form method="dialog">
+				<button class="btn btn-wide">Close</button>
+			</form>
+		</div>
+	</div>
+</dialog>
